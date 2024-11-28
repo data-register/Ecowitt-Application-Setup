71258df341489cd878152b34bb7a7e8ac9629132
data-register <58893089+data-register@users.noreply.github.com>
2024-11-28 23:34:06 +0200

    Update create_db.py

--- app/create_db.py
+++ app/create_db.py
@@ -1,14 +1,16 @@
 import psycopg2
+import os
 
 # PostgreSQL connection settings
-POSTGRES_HOST = "localhost"
-POSTGRES_PORT = "5432"
-POSTGRES_DB = "ecowitt"
-POSTGRES_USER = "ecowitt_user"
-POSTGRES_PASSWORD = "ecowitt_password"
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 # Create database and table
 try:
+    # Connect to PostgreSQL
     connection = psycopg2.connect(
         host=POSTGRES_HOST,
         port=POSTGRES_PORT,
@@ -19,7 +21,10 @@ try:
     cursor = connection.cursor()
 
     # Create the database if it doesn't exist
-    cursor.execute(f"CREATE DATABASE {POSTGRES_DB};")
+    cursor.execute(f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{POSTGRES_DB}';")
+    exists = cursor.fetchone()
+    if not exists:
+        cursor.execute(f"CREATE DATABASE {POSTGRES_DB} OWNER {POSTGRES_USER};")
     cursor.close()
     connection.close()
 
@@ -35,7 +40,7 @@ try:
 
     # Create a table to store weather data
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS weather_data (
+    CREATE TABLE IF NOT EXISTS public.weather_data (
         id SERIAL PRIMARY KEY,
         data JSONB NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
